2b26b95147e497627d0d00f0a2b3ee75d7d13881
Manvisharma-19 <manvisharma5189@gmail.com>
2026-07-19 16:34:52 +0530

    Create 1081.cpp

---
+++ Problems/Leetcode/1081.cpp
@@ -0,0 +1,28 @@
+//TC- O(n)
+//SC- O(n)
+
+class Solution {
+ public:
+  string smallestSubsequence(string text) {
+    string ans;
+    vector<int> count(128);
+    vector<bool> used(128);
+
+    for (const char c : text)
+      ++count[c];
+
+    for (const char c : text) {
+      --count[c];
+      if (used[c])
+        continue;
+      while (!ans.empty() && ans.back() > c && count[ans.back()] > 0) {
+        used[ans.back()] = false;
+        ans.pop_back();
+      }
+      used[c] = true;
+      ans.push_back(c);
+    }
+
+    return ans;
+  }
+};
