91e8593afcf55d06013a3968657a5452ca16ae90
Manvisharma-19 <manvisharma5189@gmail.com>
2026-08-23 15:21:41 +0530

    Implement sumGame function in Solution class

---
+++ Problems/Leetcode/1927. Sum Game.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.length();
+        int leftKnownSum = 0;
+        int rightKnownSum = 0;
+
+        int leftQnMarkCount = 0;
+        int rightQnMarkCount = 0;
+
+        for(int i=0; i<num.length(); i++){
+            if(num[i]=='?'){
+                if(i<n/2){
+                    leftQnMarkCount++;
+                }
+                else{
+                    rightQnMarkCount++;
+
+                }
+            }else{
+                if(i<n/2){
+                    leftKnownSum += num[i] - '0';
+                }
+                else{
+                    rightKnownSum += num[i] - '0';
+                }
+            }
+        }
+        int totalQnMarks = leftQnMarkCount + rightQnMarkCount;
+        if(totalQnMarks % 2 ==1){
+            return true;
+        }
+        int left = 2* leftKnownSum + 9*leftQnMarkCount;
+        int right = 2*rightKnownSum +
+        9*rightQnMarkCount;
+
+        if(left==right){
+            return false;
+        }
+        else{
+            return true;
+        }
+        
+    }
+};
