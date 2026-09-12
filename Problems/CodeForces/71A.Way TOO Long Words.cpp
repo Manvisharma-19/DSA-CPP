45d611de5b20cdd2357d1ab56217bda2d7d64def
Manvisharma-19 <manvisharma5189@gmail.com>
2026-09-12 15:51:24 +0530

    Add solution for Codeforces problem 71A

---
+++ Problems/CodeForces/71A.Way TOO Long Words.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+    string text[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> text[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+
+        string result = "";
+        if (text[i].length() > 10)
+        {
+            result = text[i][0] + to_string(text[i].length() - 2) + text[i][text[i].length() - 1];
+        }
+        else
+        {
+            result = text[i];
+        }
+        cout << result << endl;
+    }
+
+    puts("");
+    return 0;
+}
